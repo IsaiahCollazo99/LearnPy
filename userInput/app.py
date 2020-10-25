82559c62b53817952ec3a3b6bbcf0368f0acf247
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-10-25 12:33:55 -0400

    userInput

--- userInput/app.py
+++ userInput/app.py
@@ -1,2 +1,10 @@
-user_name = input("Enter your name: ")
-print("Hello " + user_name)
+# user_name = input("Enter your name: ")
+# print("Hello " + user_name)
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+# INT EXPECTS ONLY WHOLE NUMBERS
+# FLOAT CAN HANDLE DECIMALS
+
+print(result)
