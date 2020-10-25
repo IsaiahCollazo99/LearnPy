8339f3bdeac8b3de99590dccee0790b502b1ff0e
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-10-25 12:09:14 -0400

    work with conditional

--- variables/app.py
+++ variables/app.py
@@ -1,7 +1,13 @@
 character_name = "John"
-character_age = "35"
+character_age = 35
+isMale = False
 
 print("There once was a man named " + character_name + ", ")
-print("he was " + character_age + " years old. ")
+print("he was " + str(character_age) + " years old. ")
 print("He really liked the name " + character_name + ", ")
-print("but didn't like being " + character_age + ".")
+print("but didn't like being " + str(character_age) + ".")
+
+if(isMale):
+    print("Hey dude")
+else:
+    print("Hello *insert indetity*")
