a0a77d80e6da3e7933dcaf955d707c01960c277e
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2020-10-25 12:24:05 -0400

    messing with nums

--- variables/app.py
+++ variables/app.py
@@ -1,3 +1,5 @@
+from math import *
+
 character_name = "JOHN"
 character_age = 35
 isMale = False
@@ -7,17 +9,26 @@ isMale = False
 # print("He really liked the name " + character_name + ", ")
 # print("but didn't like being " + str(character_age) + ".")
 
-print(character_name.lower()) # To Lower Case
-print(character_name.upper()) # To Upper Case
-print(character_name.isupper()) # Is completely upper case
-print(character_name.islower()) # Is completely lower case
+# print(character_name.lower()) # To Lower Case
+# print(character_name.upper()) # To Upper Case
+# print(character_name.isupper()) # Is completely upper case
+# print(character_name.islower()) # Is completely lower case
 
-print(len(character_name)) # length
-print(character_name[0]) # Indexing into str
-print(character_name.index("J")) # Getting index of a character in str
-print(character_name.index("OH")) # Getting index where the inputted phrase starts
+# print(len(character_name)) # length
+# print(character_name[0]) # Indexing into str
+# print(character_name.index("J")) # Getting index of a character in str
+# print(character_name.index("OH")) # Getting index where the inputted phrase starts
 
-print(character_name.replace("HN", "NNY")) # Replace words/letters (first arg) with others (second arg)
+# print(character_name.replace("HN", "NNY")) # Replace words/letters (first arg) with others (second arg)
+
+# print(abs(character_age)) # Absolute value
+# print(pow(3, 2)) # Exponent 3 ^ 2
+# print(max(4, 6)) # returns which number is higher inf length
+# print(min(4, 6)) # Returns lower number inf length
+# print(round(3.7)) # rounds number
+# print(floor(3.7)) # Floors number
+# print(ceil(3.7)) # Ceils number
+# print(sqrt(64)) # Square Root
 
 # if(isMale):
 #     print("Hey dude")
